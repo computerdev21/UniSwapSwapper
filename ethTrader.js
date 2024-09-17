6f106ad84fe76a746dc38b740fe2616df1116b1a
computerdev21 <dev007chetal@gmail.com>
2024-09-17 13:08:55 -0400

    Changes

---
+++ ethTrader.js
@@ -0,0 +1,111 @@
+const ethers = require('ethers');
+const { JsonRpcProvider } = require('@ethersproject/providers');
+const { abi: IUniswapV3PoolABI } = require('@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json');
+const { abi: SwapRouterABI } = require('@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json');
+const { getPoolImmutables, getPoolState } = require('./helpers');
+const ERC20ABI = require('./abi.json');
+const cron = require('node-cron');
+const sqlite3 = require('sqlite3').verbose();
+require('dotenv').config();
+
+const INFURA_URL_TESTNET = process.env.INFURA_URL_TESTNET;
+
+if (!INFURA_URL_TESTNET) {
+    console.error("Please ensure .env file has INFURA_URL_TESTNET defined");
+    process.exit(1);
+}
+
+const provider = new JsonRpcProvider(INFURA_URL_TESTNET);
+
+const poolAddress = "0x7caa81b917dfdc83943bf99a1f3c947f849658d4"; // WLFI/WETH pool address
+const swapRouterAddress = '0xE592427A0AEce92De3Edee1F18E0157C05861564';
+
+const name0 = 'Ether';
+const symbol0 = 'ETH';
+const decimals0 = 18;
+const address0 = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'; // ETH on Ethereum
+
+const name1 = 'World Liberty Financial';
+const symbol1 = 'WLFI';
+const decimals1 = 18;
+const address1 = '0xC9bd9b4F5e96644B9C5C525E99f655Ff31ce5Ce6'; // WLFI on ETH
+
+async function performSwap(walletAddress, walletSecret) {
+    console.log(`Starting script for wallet: ${walletAddress}...`);
+
+    const poolContract = new ethers.Contract(poolAddress, IUniswapV3PoolABI, provider);
+
+    try {
+        // Test basic interaction with the contract
+        const slot0 = await poolContract.slot0();
+        console.log("Slot0:", slot0);
+
+        const immutables = await getPoolImmutables(poolContract);
+        const state = await getPoolState(poolContract);
+
+        console.log("Pool Immutables:", immutables);
+        console.log("Pool State:", state);
+
+        const wallet = new ethers.Wallet(walletSecret);
+        const connectedWallet = wallet.connect(provider);
+
+        const swapRouterContract = new ethers.Contract(swapRouterAddress, SwapRouterABI, provider);
+
+        const inputAmount = 0.00005;
+        const amountIn = ethers.utils.parseUnits(inputAmount.toString(), decimals0);
+
+        const approvalAmount = amountIn.mul(10).toString();
+        const tokenContract0 = new ethers.Contract(address0, ERC20ABI, provider);
+
+        console.log("Approving token...");
+        const approvalResponse = await tokenContract0.connect(connectedWallet).approve(swapRouterAddress, approvalAmount);
+        console.log("Approval response:", approvalResponse);
+
+        const params = {
+            tokenIn: immutables.token1,
+            tokenOut: immutables.token0,
+            fee: immutables.fee,
+            recipient: walletAddress,
+            deadline: Math.floor(Date.now() / 1000) + (60 * 10),
+            amountIn: amountIn,
+            amountOutMinimum: 0,
+            sqrtPriceLimitX96: 0,
+        };
+
+        console.log("Executing swap with params:", params);
+
+        const transaction = await swapRouterContract.connect(connectedWallet).exactInputSingle(params, {
+            gasLimit: ethers.utils.hexlify(10000000)
+        });
+        console.log("Transaction:", transaction);
+    } catch (error) {
+        console.error("Detailed error:", JSON.stringify(error, null, 2));
+        console.error("Error message:", error.message);
+        console.error("Error code:", error.code);
+        console.error("Error data:", error.data);
+    }
+}
+
+async function fetchWalletsAndPerformSwaps() {
+    const db = new sqlite3.Database('./wallets.db');
+
+    db.serialize(() => {
+        db.each("SELECT address, secret FROM wallets", async (err, row) => {
+            if (err) {
+                console.error("Error fetching wallet data:", err);
+                return;
+            }
+            await performSwap(row.address, row.secret);
+        });
+    });
+
+    db.close();
+}
+
+cron.schedule('* * * * *', () => {
+    fetchWalletsAndPerformSwaps().catch(error => {
+        console.error("Error in fetchWalletsAndPerformSwaps function:", error);
+    });
+});
+
+console.log("Bot started, running every minute...");
--- setupDatabase.js
+++ setupDatabase.js
@@ -5,7 +5,24 @@ db.serialize(() => {
     db.run("CREATE TABLE IF NOT EXISTS wallets (id INTEGER PRIMARY KEY, address TEXT, secret TEXT)");
 
     const stmt = db.prepare("INSERT INTO wallets (address, secret) VALUES (?, ?)");
-    stmt.run('0xYourWalletAddress1', 'yourwalletsecret1');
+
+    const wallets = [
+        {address: '0x238d993C808f02eD53607aC7928013d33F2dc177', secret: '80b9614a897296a8c972bfd3772efad95e025a336de9a99eb53439e257ba6067'},
+        {address: '0xe0F86a49c33343E2B54beebd2B0C2d8406DDAe11', secret: '2f5f3128697bc6fa0ce0c90179c528d6c80ee73d784d6d95e8a3e8e2dd48bd2a'},
+        {address: '0x2B7150648d9E0781aADFEB167c4380E6098E040f', secret: '7a473d93d2dd0fb30d9771df8dc111c45c96472a676ec269c3bd06118800f08c'},
+        {address: '0xd09b9977457F70Fff5A0a8bB783dC73d717E8808', secret: '85a97e271a2f05a32a07ef649cf5aab791fd18fd674515df8688b0f401beb6f6'},
+        {address: '0xd2bCD606408755D421e2970fE6711D0151A5e2b4', secret: 'b728f86ccc99552c0604341fcdc4db85ffe75e938e7e95c7a99a0c298a12878e'},
+        {address: '0xf2FEfb5B25681a4491a51f4c622C883BE42dcf85', secret: '5c73fd5a4a999ecd5dfa06e27e3ac5364577269964177d631f9b208bc0005786'},
+        {address: '0x7E02De4D6D9e587E9FbBfbeF5cC3Afe7c6137067', secret: '2a72634c9115b5dee85b65da3c5f8ed76d6a788d67f73fee02954c4024d03cd5'},
+        {address: '0x6aa067733Df43d6892C4dd6A54F1BD889Aef31FE', secret: '08453ae0d13280b3e72c682f886d27d393c2ec1d587a28d31445e9eb1e3c7c33'},
+        {address: '0x89DCfc202D5f5C047500c10Da7Bb520b70904699', secret: 'b054140fc316e313d85f4e5f42e0dab6e5462caa956c22830737f79bcf34fd1a'},
+        {address: '0x0b642dba3f8b9029152248bB057Cc69F39Cb56F6', secret: '6fc9531ba961790a13d6f6b10d872fa2c331753fecbec8681203aee77b958d9d'}
+    ];
+
+    wallets.forEach(wallet => {
+        stmt.run(wallet.address, wallet.secret);
+    });
+
     stmt.finalize();
 });
 
